e8b82a2d97ad259e8c37fb44cb841008f235f135
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    sepolicy: Initial repo structure

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from ZenOS
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/zen/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from ZenOS
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/zen/sepolicy/qcom
