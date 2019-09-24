46d9502a82c44b2290c3f4e6c861dfbdf4ff78aa
ujwalp15 <ujwalp09@gmail.com>
2019-09-24 03:46:14 -0400

    sepolicy: common: inherit dynamic sepolicy
    This was added for fod, but never inherited in zen since we havent used it before.
    
    Change-Id: Iac1aee8fc006ff1d00301b765333102147461d17

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -3,11 +3,23 @@
 # inherit from ZenOS
 #
 
+ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
+ifeq ($(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
     device/zen/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/zen/sepolicy/common/private
 
+ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/zen/sepolicy/common/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
+    device/zen/sepolicy/common/dynamic \
     device/zen/sepolicy/common/vendor
+endif
